889e75634a81984873601b4c78d8da1d03d77290
andwis93 <andwis17@gmail.com>
2022-10-12 18:26:21 +0200

    15.1 Singleton sluzacy do zapisywania logow z wykonanych operacji

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+    private static Logger logger;
+
+    @BeforeAll
+    public static void getLogger() {
+        logger = Logger.LOG;
+        logger.log("Checking last log");
+    }
+
+    @Test
+    void testGetLastLogger() {
+        //Given
+
+        //When
+        String lastLog = logger.getLastLog();
+
+        //Then
+        assertEquals("Checking last log", lastLog);
+    }
+
+}
